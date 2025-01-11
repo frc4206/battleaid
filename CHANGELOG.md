7ccfc04547c9482f9e2b49f8c8acdc23df2adc1c
cmoliverio <cmoliverio.10@gmail.com>
2025-01-11 14:19:13 -0600

    Fixed to TunedJoystick, moved to 2025 libraries.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,14 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## [0.2.0] - 2025-01-11
+
+### Fixed
+- Improper scaling in `TunedJoystick` due to not using polar coordinates.  `TunedJoystick` now uses polar coordinates with a periodicity to reduce computational overhead.  
+
+### Changed
+- Moved from `2024.3.2` libraries to `2025.2.1`.
+
 ## [0.1.2] - 2024-12-03
 
 ### Fixed
--- src/main/java/org/team4206/battleaid/common/TunedJoystick.java
+++ src/main/java/org/team4206/battleaid/common/TunedJoystick.java
@@ -11,6 +11,15 @@ public final class TunedJoystick {
     private XboxController cntrllr;
     private ResponseCurve rc;
 
+    private int periodMilliseconds = 20;
+    private long t1_lft = 0; // timestamp of previous check
+    private long t1_rght = 0;
+
+    private double lx = 0.0d;
+    private double ly = 0.0d;
+    private double rx = 0.0d;
+    private double ry = 0.0d;
+
     /**
      * Helper class for applying exponential transformations.
      */
@@ -56,6 +65,15 @@ public final class TunedJoystick {
         return ((val - in_min) * (out_max - out_min) / (in_max - in_min)) + out_min;
     }
 
+    /**
+     * Sets the period on which TunedJoystick performs
+     * deadzone and scaling operations. Default is 20ms.
+     * Only use this if you know what you're doing.
+     */
+    void setPeriodMilliseconds(int milliseconds) {
+        this.periodMilliseconds = milliseconds;
+    }
+
     /*
      * Returns 0 if input is lower than deadzone.
      * Otherwise, deadzone is the new '0' and scales to the max value (of 1.0)
@@ -91,36 +109,81 @@ public final class TunedJoystick {
      * in order to reduce internal branching.
      */
     double tune(double i) {
-        double result = rc.applyCurve(applyDeadzone(Math.abs(i)));
-        return (i >= 0.0d) ? result : result * -1.0d;
+        return (i < 0.0d) ? 0.0 : rc.applyCurve(applyDeadzone(i));
+    }
+
+    void tuneLeftStick() {
+        long t2 = System.currentTimeMillis();
+
+        if (t2 - t1_lft < periodMilliseconds)
+            return;
+
+        t1_lft = t2;
+
+        double _lx = cntrllr.getLeftX();
+        double _ly = cntrllr.getLeftY();
+
+        // convert to polar
+        double r = Math.sqrt((_lx * _lx) + (_ly * _ly));
+        double theta = Math.atan2(_ly, _lx);
+
+        r = tune(r);
+
+        this.lx = r * Math.cos(theta);
+        this.ly = r * Math.sin(theta);
+    }
+
+    void tuneRightStick() {
+        long t2 = System.currentTimeMillis();
+
+        if (t2 - t1_rght < periodMilliseconds)
+            return;
+
+        t1_rght = t2;
+
+        double _rx = cntrllr.getRightX();
+        double _ry = cntrllr.getRightY();
+
+        // convert to polar
+        double r = Math.sqrt((_rx * _rx) + (_ry * _ry));
+        double theta = Math.atan2(_ry, _rx);
+
+        r = tune(r);
+
+        this.rx = r * Math.cos(theta);
+        this.ry = r * Math.sin(theta);
     }
 
     /**
      * @return Tuned X axis of left joystick.
      */
     public double getLeftX() {
-        return tune(cntrllr.getLeftX());
+        tuneLeftStick();
+        return lx;
     }
 
     /**
      * @return Tuned Y axis of left joystick.
      */
     public double getLeftY() {
-        return tune(cntrllr.getLeftY());
+        tuneLeftStick();
+        return ly;
     }
 
     /**
      * @return Tuned X axis of right joystick.
      */
     public double getRightX() {
-        return tune(cntrllr.getRightX());
+        tuneRightStick();
+        return rx;
     }
 
     /**
      * @return Tuned Y axis of right joystick.
      */
     public double getRightY() {
-        return tune(cntrllr.getRightY());
+        tuneRightStick();
+        return ry;
     }
 
 }
